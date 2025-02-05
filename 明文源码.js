8feca4fc214216a8cfec0d4dd388d0dac6ad4fba
刘宇森 <102225984+PythonDataAnalysiser@users.noreply.github.com>
2025-02-06 04:00:38 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,8 +1,8 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
-let proxyIP = '';
+let userID = '4b3e8eda-c80b-47c3-8c59-9d88a6cbac76';
+let proxyIP = 'https://d3f9a417.jego.vip/subscribe/v2rayn/d8c778b38612c37fe1274d1129e78270';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLmZ4eGsuZGVkeW4uaW8=');
 let subConfig = atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FDTDRTU1IvQUNMNFNTUi9tYXN0ZXIvQ2xhc2gvY29uZmlnL0FDTDRTU1JfT25saW5lX01pbmlfTXVsdGlNb2RlLmluaQ==');
